91a766590e4a98180ba73eb6d6458efcdbd026b3
Harshilshah11 <shahharshil054@gmail.com>
2026-09-15 14:42:14 +0530

    Latest Updated Code

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -1,10 +1,10 @@
-import HeroSection from '@/components/sections/home/HeroSection';
-import AboutSection from '@/components/sections/home/AboutSection';
-import ProductsSection from '@/components/sections/home/ProductsSection';
-import EnvironmentSection from '@/components/sections/home/EnvironmentSection';
-import RecognitionSection from '@/components/sections/home/RecognitionSection';
-import IndustriesSection from '@/components/sections/home/IndustriesSection';
-import Cta from '@/components/sections/Cta';
+import HeroSection from "@/components/sections/home/HeroSection";
+import AboutSection from "@/components/sections/home/AboutSection";
+import ProductsSection from "@/components/sections/home/ProductsSection";
+import EnvironmentSection from "@/components/sections/home/EnvironmentSection";
+import RecognitionSection from "@/components/sections/home/RecognitionSection";
+import IndustriesSection from "@/components/sections/home/IndustriesSection";
+import Cta from "@/components/sections/Cta";
 
 /** Port of index.php */
 export default function HomePage() {
@@ -23,16 +23,6 @@ export default function HomePage() {
       <RecognitionSection />
       <IndustriesSection />
       <Cta />
-      <nav >
-  <div >My Website</div>
-
-  <div >
-    <a href="#">Home</a>
-    <a href="#">About</a>
-    <a href="#">Services</a>
-    <a href="#">Contact</a>
-  </div>
-</nav>
     </main>
   );
 }
